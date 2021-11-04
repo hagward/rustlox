b43b2a432baad3069954e0919cc2cec748126b33
Anders Hagward <taperlawyer@gmail.com>
2021-11-04 22:12:45 +0100

    Implement constants

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
-const OP_RETURN: u8 = 0;
+const OP_CONSTANT: u8 = 0;
+const OP_RETURN: u8 = 1;
+
+type Value = f64;
 
 struct Chunk {
     code: Vec<u8>,
+    constants: Vec<Value>,
 }
 
 impl Chunk {
+    fn new() -> Self {
+        Self {
+            code: Vec::new(),
+            constants: Vec::new(),
+        }
+    }
+
+    fn add_constant(&mut self, value: Value) -> usize {
+        self.constants.push(value);
+        self.constants.len() - 1
+    }
+
+    fn write_chunk(&mut self, byte: u8) {
+        self.code.push(byte);
+    }
+
     fn disassemble_chunk(&self, name: &str) {
         println!("== {} ==", name);
 
@@ -15,16 +35,24 @@ impl Chunk {
     }
 
     fn disassemble_instruction(&self, offset: usize) -> usize {
-        print!("{number:0>width$} ", number = offset, width = 4);
+        print!("{:04} ", offset);
 
         let instruction = self.code[offset];
         match instruction {
+            OP_CONSTANT => self.constant_instruction("OP_CONSTANT", offset),
             OP_RETURN => self.simple_instruction("OP_RETURN", offset),
             _ => {
                 println!("Unknown opcode {}", instruction);
                 offset + 1
-            },
+            }
         }
+    }
+
+    fn constant_instruction(&self, name: &str, offset: usize) -> usize {
+        let constant_index = self.code[offset + 1];
+        let constant_value = self.constants[constant_index as usize];
+        println!("{} {:4} '{}'", name, constant_index, constant_value);
+        offset + 2
     }
 
     fn simple_instruction(&self, name: &str, offset: usize) -> usize {
@@ -34,9 +62,13 @@ impl Chunk {
 }
 
 fn main() {
-    let chunk = Chunk {
-        code: vec![OP_RETURN],
-    };
+    let mut chunk = Chunk::new();
+    let mut constant = chunk.add_constant(1.2);
+    chunk.write_chunk(OP_CONSTANT);
+    chunk.write_chunk(constant as u8);
+    chunk.write_chunk(OP_RETURN);
+    constant = chunk.add_constant(12345.0);
+    chunk.write_chunk(OP_CONSTANT);
+    chunk.write_chunk(constant as u8);
     chunk.disassemble_chunk("test chunk");
 }
-
